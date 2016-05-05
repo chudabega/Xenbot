9e230922453327aa6a7ed675e0e44bd23a999f92
chudabega <chudabega@hotmail.com>
2016-05-05 19:16:04 -0400

    Recursive containers implemented with dragula

--- main.js
+++ main.js
@@ -11,16 +11,60 @@ if (Meteor.isClient) {
 		}
 	});
 	Template.dragList.onRendered(function(){
-		dragula([document.getElementById('left1'), document.getElementById('right1')], {
-			copy: function (el, source) {
-				return source === document.getElementById('left1')
-			},
-			accepts: function (el, target) {
-				return target !== document.getElementById('left1')
-			},
-			removeOnSpill: true
-		});
+		var drake = setupDragula();
+
+		drake.on('dragend', function (el) {recursiveSetup(el)});
+
+		function recursiveSetup(el) {
+			if (el.className.indexOf('row') > -1) {
+				drake.destroy();
+				drake = setupDragula();
+				drake.on('dragend', function(e) {recursiveSetup(e)});
+			}
+		}
+
+		// Dragula setup
+		function setupDragula() {
+			return dragula(querySelectorAllArray('.container'),{
+				copy: function (el, source) {
+					return source === document.getElementById('left1')
+				},
+			    accepts: function (el, target, source, sibling) {
+			        // prevent dragged containers from trying to drop inside itself
+					var copyEle = document.getElementById('left1');
+
+			        return !contains(el,target) && target !== copyEle
+			        	&& !isDescendant(copyEle, target);
+			    },
+			    removeOnSpill: true
+			});
+		}
+
+		function isDescendant(parent, child) {
+		     var node = child.parentNode;
+		     while (node != null) {
+		         if (node == parent) {
+		             return true;
+		         }
+		         node = node.parentNode;
+		     }
+		     return false;
+		}
+		// UTILS
+		function querySelectorAllArray(selector){
+		    return Array.prototype.slice.call(
+		        document.querySelectorAll(selector), 0
+		    );
+		}
+		// http://ejohn.org/blog/comparing-document-position/
+		function contains(a, b){
+		  return a.contains ?
+		    a != b && a.contains(b) :
+		    !!(a.compareDocumentPosition(b) & 16);
+		}
 	});
+
+
 }
 
 if (Meteor.isServer) {
