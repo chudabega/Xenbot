2cb320b32290a9ce8a21548d10736ce71c90853d
chudabega <chudabega@hotmail.com>
2016-05-06 13:18:11 -0400

    Support for multiple selection, removal, copy and move.

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ if (Meteor.isClient) {
 			return Items.find({});
 		}
 	});
-	Template.dragList.onRendered(function(){
-		var drake = setupDragula();
+	Template.dragList.onRendered(function() {
+		var debug = false;
 
-		drake.on('dragend', function (el) {recursiveSetup(el)});
+		var drake = setupDragula();
+		var selectStack = [];
+		var dragEle;
+		var copyBool;
+		var removeEle;
+		var selectedPositions = $([]);
+
+		drake.on('dragend', function(el) {recursiveSetup(el)});
 
 		function recursiveSetup(el) {
+			moveSelected(el);
 			if (el.className.indexOf('row') > -1) {
 				drake.destroy();
 				drake = setupDragula();
@@ -23,21 +31,151 @@ if (Meteor.isClient) {
 			}
 		}
 
+		$(window).on('mousedown', function(e) {select(e);});
+
+		drake.on('remove', function() {removeAllOthers();});
+
 		// Dragula setup
 		function setupDragula() {
 			return dragula(querySelectorAllArray('.container'),{
 				copy: function (el, source) {
-					return source === document.getElementById('left1')
+					copyBool = source === document.getElementById('left1');
+					dragEle = el;
+
+					//saving the original relative positions of the elements before they are moved
+					if (!copyBool) {
+						selectedPositions = $('[grabbed]');
+					}
+					
+					return copyBool;
 				},
 			    accepts: function (el, target, source, sibling) {
 			        // prevent dragged containers from trying to drop inside itself
-					var copyEle = document.getElementById('left1');
+					var copySource = document.getElementById('left1');
 
-			        return !contains(el,target) && target !== copyEle
-			        	&& !isDescendant(copyEle, target);
+			        return !contains(el,target) && target !== copySource
+			        	&& !isDescendant(copySource, target);
 			    },
 			    removeOnSpill: true
 			});
+		}
+
+		function removeAllOthers() {
+			$('[grabbed]').each(function() {
+				if (this.className.indexOf('gu-mirror') == -1) {
+					this.remove();
+				}
+			});
+		}
+
+		function select(e) {
+			//TODO prevent shift modifier on cross container
+			var target = e.target;
+			var className = target.className;
+			if (className.indexOf('row') > -1 || className.indexOf('el') > -1) {
+				if (e.shiftKey) {
+					if (selectStack.length > 0) {
+						var lastTarget = selectStack[selectStack.length - 1];
+						if (target === lastTarget) {
+							return;
+						}
+						
+						var container = $('.container');
+						var targetInd = container.find(target).index();
+						var lastTargetInd = container.find(lastTarget).index();
+
+						//If the target is after the last target in the page use next else use prev
+						if (targetInd > lastTargetInd) {
+							$(lastTarget).nextUntil(target).attr('grabbed', 'true');
+						} else {
+							$(lastTarget).prevUntil(target).attr('grabbed', 'true');
+						}
+					}
+
+					//Remove the previous index of the target in the stack if it exists.
+					var ind = selectStack.indexOf(target);
+					if (ind > -1) {
+						selectStack.splice(ind, 1);
+					}
+
+					target.setAttribute('grabbed', 'true');
+					selectStack.push(target);
+				} else if (e.ctrlKey) {
+					//If grabbed
+					if (target.getAttribute('grabbed')) {
+						target.removeAttribute('grabbed');
+
+						//remove the target from the select stack
+						var ind = selectStack.indexOf(target);
+						if (ind > -1) {
+							selectStack.splice(ind, 1);
+						}
+					//If not grabbed
+					} else {
+						target.setAttribute('grabbed', 'true');
+						selectStack.push(target);
+					}
+				//no modifier
+				} else {
+					//if not grabbed
+					if (!target.getAttribute('grabbed')) {
+						$('[grabbed]').removeAttr('grabbed');
+						target.setAttribute('grabbed', 'true');
+
+						selectStack = [];
+						selectStack.push(target);
+					}
+				}
+			} else {
+				if (!(e.shiftKey || e.ctrlKey)) {
+					$('[grabbed]').removeAttr('grabbed');
+					selectStack = [];
+				}
+			}
+			if (debug) {console.log(selectStack);}
+		}
+
+		function moveSelected(target) {
+			if (copyBool) {
+				$(target).removeAttr('grabbed');
+			}
+
+			var before = true;
+			var ele;
+			var targetInd = selectedPositions.index(target);
+			$('[grabbed]').each(function() {
+				if (copyBool) {
+					if (this === dragEle) {
+						before = false;
+						return;
+					}
+				} else {
+					if (this === target) {
+						return;
+					}
+
+					before = true;
+					if (selectedPositions.index(this) > targetInd) {
+						before = false;
+					}
+				}
+
+				if (copyBool) {
+					ele = $(this).clone();
+					ele.removeAttr('grabbed');
+				} else {
+					ele = $(this);
+				}
+
+				if (before) {
+					ele.insertBefore(target);
+				} else {
+					ele.insertAfter(target);
+				}
+			});
+
+			dragEle = null;
+			selectedPositions = $([]);
 		}
 
 		function isDescendant(parent, child) {
